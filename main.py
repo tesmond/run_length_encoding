b1380f922d8394cc9ca7036c7c23878874edbd2c
Andrew Gill <tesmond@gmail.com>
2022-06-06 00:47:06 +0100

    simplify regex and add performance testing

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import re
 
-
-def encode(input):
+def encode(input: str) -> str:
     # regex to group sequencial matching characters using back reference
     regex = r"((.)(\2*))"
-    output = ""
+    output: str = ""
     # iterate the groups and build the output string
     for g in re.finditer(regex, input):
-        number = len(g.group()) if len(g.group()) > 1 else ""
-        output += str(number) + g.group()[-1]
+        group: str = g.group()
+        number: str  = str(len(group)) if len(group) > 1 else ""
+        output += number + group[-1]
     return output
 
 
-def decode(input):
-    # regex to group sequences of 0 or more numbers and any alpha character or whitespace
-    regex = r"([0-9]*[A-Za-z ]{1})"
-    output = ""
+def decode(input: str) -> str:
+    # regex to group sequences of 0 or more digits and any non-digit character
+    regex = r"([\d]*[\D]{1})"
+    output: str = ""
     # iterate the groups and build the output string
     for g in re.finditer(regex, input):
-        total_chars = len(g.group())
-        times = g.group()[:-1] if total_chars > 1 else 1
-        output += g.group()[-1] * int(times)
+        group: str = g.group()
+        times: int = int(group[:-1]) if len(group) > 1 else 1
+        output += group[-1] * times
     return output
